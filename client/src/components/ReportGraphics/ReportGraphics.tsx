3ee6dbc977f497b56bdbe05282e292852e82a3a5
Kukonen <ekukonen@gmail.com>
2024-03-16 09:39:46 +0300

    fix: fix lable text

--- client/src/components/ReportGraphics/ReportGraphics.tsx
+++ client/src/components/ReportGraphics/ReportGraphics.tsx
@@ -89,7 +89,7 @@ const ReportGraphics = ({serviceNumber} : ReportProps) => {
             datasets: [
                 {
                     type: 'line' as const,
-                    label: 'Баллы',
+                    label: 'Зарплата',
                     data: yData,
                     borderColor: 'rgb(53, 162, 235)',
                     backgroundColor: 'rgba(53, 162, 235, 0.5)',
